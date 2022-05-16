2d03d2076ef05b3cac9d320b38686473a202392b
Kayla <kbaistrocchi@gmail.com>
2022-05-15 21:41:41 -0400

    Add basic html structure to posts

--- themes/trader/single.php
+++ themes/trader/single.php
@@ -1,12 +1,16 @@
 <?php get_header(); ?>
 
+<main>
 <?php
 if(have_posts() ) :
     // THE WP LOOP
     while( have_posts() ) :
         the_post(); ?>
-<h2><?php the_title(); ?></h2>
-        <?php the_content(); ?>
+        <h2><?php the_title(); ?></h2>
+        <p><em>written by: <?php echo get_the_author(); ?></em></p>
+        <section>
+            <?php the_content(); ?>
+        </section>
 
     <!-- end of wp loop -->
     <?php endwhile; ?>
@@ -16,5 +20,6 @@ if(have_posts() ) :
 <?php else : ?>
         <p>No posts found</p>
 <?php endif; ?>
+</main>
 
 <?php get_footer(); ?>
